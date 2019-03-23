2b13f752089e049d09b0cc430663d3a9fb83c4a8
Roman Melnyk <email.rom.melnyk@gmail.com>
2019-03-23 23:34:09 +0100

    02-implicit-typing*

---
+++ samples/02-implicit-typing-any.ts
@@ -0,0 +1,4 @@
+const array = [0, false, 'test', {}];
+const el = array[ Math.floor(Math.random() * array.length) ];
+
+console.log(el[0]);
---
+++ samples/02-implicit-typing.ts
@@ -0,0 +1,2 @@
+const n = Math.floor(Math.random() * 10);
+console.log(n.length);
