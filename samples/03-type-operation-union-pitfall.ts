1d14c493c273911e85ebf7b3c5437e92b8b2ba06
Roman Melnyk <rmelnyk@thepeoplegroup.com>
2019-04-12 15:48:50 +0300

    03-type-operation-union-pitfall.ts

---
+++ samples/03-type-operation-union-pitfall.ts
@@ -0,0 +1,20 @@
+type Dog = { bark: () => string; };
+type Cat = { purr: () => string; };
+type MyPet = { name: string } & (Dog | Cat);
+
+const dog: MyPet = { bark: () => 'Woff!', name: 'Good boy', };
+const cat: MyPet = { bark: () => 'Meow!', name: 'Lucky', };
+
+function getMyPet(name: string): (MyPet | null) {
+  return [dog, cat].find((myPet: MyPet) => myPet.name === name) || null;
+}
+
+const pet = getMyPet('Lucky');
+
+// Following causes 2 errors:
+//   Object is possibly 'null'
+//   Property 'purr' does not exist on type 'MyPet'. Property 'purr' does not exist on type '{ name: string; } & Dog'
+pet.purr();
+
+// Following works:
+(pet as Cat).purr();
