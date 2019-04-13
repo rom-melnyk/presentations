b7d889b5aaf263c0c2fdc0a953bd931ed7aeff95
Roman Melnyk <rmelnyk@thepeoplegroup.com>
2019-04-13 09:08:35 +0300

    Add comments fo failing 04-solid-open-close.ts

--- samples/04-solid-open-close.ts
+++ samples/04-solid-open-close.ts
@@ -1,11 +1,15 @@
 type Dog = { bark: () => string; };
 
+// TS2420: Class 'MyDog' incorrectly implements interface 'Dog'.
+//   Property 'bark' is protected in type 'MyDog' but public in type 'Dog'.
 class MyDog implements Dog {
   protected bark(): string {
     return 'Woff!';
   }
 }
 
+// TS2415: Class 'MyOtherDog' incorrectly extends base class 'MyDog'.
+//   Property 'bark' is private in type 'MyOtherDog' but not in type 'MyDog'.
 class MyOtherDog extends MyDog {
   private bark(): string {
     return 'Woff!';
